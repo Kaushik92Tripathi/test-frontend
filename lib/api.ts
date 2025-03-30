a12912383474b1cf571f49aa8f6eba0f4898d2e1
Kaushik92Tripathi <kaushik.tripathi@tothenew.com>
2025-03-30 14:25:41 +0530

    changed env variable

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Doctor, Specialty } from "@/types/doctor";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_URL,
