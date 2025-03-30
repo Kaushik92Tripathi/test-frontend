b74680c2ab3e55c8230188b8707f7469b767c199
Kaushik92Tripathi <kaushik.tripathi@tothenew.com>
2025-03-30 15:36:48 +0530

    changed env variable

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -27,7 +27,7 @@ export default function Login() {
     console.log(email, password);
 
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/google`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
